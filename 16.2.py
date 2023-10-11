e45e79ebbe79da754e0f19191ae5bd85f6f4bbe0
StudentSYN40 <125072826+StudentSYN40@users.noreply.github.com>
2023-10-11 18:03:51 +0300

    Update 16.2.py

--- 16.2.py
+++ 16.2.py
@@ -28,7 +28,10 @@ class Turtle:
     def count_moves(self, x2, y2):
         x_diff = abs(self.x - x2)
         y_diff = abs(self.y - y2)
-        return (x_diff + y_diff) // self.s
+        if x_diff % self.s == 0 and y_diff % self.s == 0:
+            return (x_diff + y_diff) // self.s
+        else:
+            return "Невозможно достичь указанной точки с текущим размером шага"
 
 turtle = Turtle(0, 0, 2)
 
@@ -42,7 +45,16 @@ print(turtle.x, turtle.y, turtle.s)
 turtle.degrade()
 print(turtle.s)
 
+print(f"Текущая позиция: {turtle.x, turtle.y}")
 x2 = 3
 y2 = 4
 moves = turtle.count_moves(x2, y2)
-print(f"Для достижения точки ({x2}, {y2}) потребуется {moves} ходов")
+print(moves)
+
+turtle.degrade()
+turtle.go_up()
+turtle.go_right()
+turtle.evolve()
+print(f"Текущая позиция: {turtle.x, turtle.y}")
+print(turtle.count_moves(3, 4))
+print(turtle.count_moves(4, 4))
